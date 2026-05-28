5604d19414d16d9d51bf9a91cafe47ce0e966891
hoangcong97 <hoangcong110297@gmail.com>
2026-05-28 16:52:27 +0700

    Fix socket

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-// export const socket = io("https://chat-app-7vt9.onrender.com");
-export const socket = io("http://localhost:5000");
+export const socket = io("https://chat-app-7vt9.onrender.com");
+// export const socket = io("http://localhost:5000");
