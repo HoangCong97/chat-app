dcd4b87f96cd9ce45666f69a11367c496b8d8d50
hoangcong97 <hoangcong110297@gmail.com>
2026-05-29 17:20:31 +0700

    Thêm nghiệp vụ lập trình đồng bộ API

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,20 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // host: process.env.DB_HOST,
-  // port: process.env.DB_PORT,
-  // database: process.env.DB_NAME,
-
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+      },
+);
 
 module.exports = pool;
